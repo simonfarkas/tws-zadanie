b0d385c68cdf804f7e2ff8bc57f0bc2e52111a8d
simonfarkas <simon.farkas0@gmail.com>
2022-12-14 19:48:48 +0100

    WIP

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,11 +7,9 @@ export const Footer = () => {
       bg="primary.1"
       color="white"
       p={10}
-      h={32}
       position="fixed"
       w="100%"
       bottom={0}
-      mt="auto"
     >
       <Container maxW="6xl">
         <Text align="center">Simon Farkas &#169; 2022 </Text>
--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -62,6 +62,26 @@ export const About = () => {
           fugiat in incidunt ipsa laboriosam libero minima odit officiis
           pariatur placeat possimus qui quisquam reiciendis rem repellendus sit
           tenetur totam vel veniam voluptas voluptates.
+        </Text>{" "}
+        <Text
+          fontFamily="Tangerine"
+          fontStyle="italic"
+          fontSize="4xl"
+          fontWeight={600}
+          align="left"
+          color="white"
+          pt={10}
+        >
+          development history
+        </Text>
+        <Text color="white" align="left" pt={10} maxW="2xl">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+          consectetur dicta dolorum nesciunt possimus quae rerum sapiente velit
+          veniam. Aliquid at deleniti earum exercitationem iste laborum odio
+          ratione sit tempora. Accusamus ad alias, aliquam consequuntur deleniti
+          fugiat in incidunt ipsa laboriosam libero minima odit officiis
+          pariatur placeat possimus qui quisquam reiciendis rem repellendus sit
+          tenetur totam vel veniam voluptas voluptates.
         </Text>
       </Container>
     </Flex>
