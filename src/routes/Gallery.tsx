d68f12ab4a6d1d3ad142b712b2bdc3191c511c97
simonfarkas <simon.farkas0@gmail.com>
2022-12-23 22:12:36 +0100

    Change gallery

--- src/routes/Gallery.tsx
+++ src/routes/Gallery.tsx
@@ -1,46 +1,71 @@
-import React from "react";
-import { Text } from "@chakra-ui/react";
-import { Grid, GridItem } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Box, Text } from "@chakra-ui/react";
+import { SimpleGrid, Flex } from "@chakra-ui/react";
+
+const photos = [
+  { src: "/public/1-compressed.png", title: "wellnesso.sk" },
+  { src: "/public/2-compressed.png", title: "RemindeMe" },
+];
 
 export const Gallery = () => {
+  const [currentImage, setCurrentImage] = useState<string>(photos[0].src);
+  const [isOverlay, setIsOverlay] = useState<{
+    bool: boolean;
+    id: number | null;
+  }>({ bool: false, id: null });
+
+  const handleClick = (id: number) => {
+    setCurrentImage(photos[id].src);
+  };
+
   return (
-    <Grid templateColumns="repeat(2, 1fr)">
-      <GridItem>
-        <a href="/public/1-compressed.png" target="_blank">
-          <img
-            alt="logo"
-            src="/public/1-compressed.png"
-            width={360}
-            height={240}
-            style={{ height: 240 }}
-          />
-          <Text color="white" align="center" mt={2}>
-            <a href="https://wellnesso.sk/" target="_blank">
-              wellnesso.sk
-            </a>
-          </Text>
-        </a>
-      </GridItem>
-      <GridItem>
-        <a href="/public/2-compressed.png" target="_blank">
-          <img
-            alt="logo"
-            src="/public/2-compressed.png"
-            width={360}
-            height={240}
-            style={{
-              objectFit: "cover",
-              height: 240,
-              objectPosition: "top",
-            }}
-          />
-          <Text color="white" align="center" mt={2}>
-            <a href="https://github.com/simonfarkas/hk2022" target="_blank">
-              RemindMe
-            </a>
-          </Text>
-        </a>
-      </GridItem>
-    </Grid>
+    <Flex direction="column">
+      <img
+        alt="project"
+        src={currentImage}
+        width={700}
+        style={{
+          height: 400,
+          borderRadius: "5px",
+        }}
+      />
+      <SimpleGrid columns={[1, null, 2]} mt={10}>
+        {photos.map((photo, index) => (
+          <Box
+            key={index}
+            position="relative"
+            onMouseOver={() => setIsOverlay({ bool: true, id: index })}
+            onMouseLeave={() => setIsOverlay({ bool: false, id: index })}
+          >
+            <img
+              alt="project"
+              src={photo.src}
+              width={350}
+              style={{
+                height: "100%",
+                filter: `${
+                  isOverlay.bool && isOverlay.id === index
+                    ? "brightness(50%)"
+                    : ""
+                }`,
+              }}
+              onClick={() => handleClick(index)}
+            />
+            <Text
+              display={
+                isOverlay.bool && isOverlay.id === index ? "block" : "none"
+              }
+              color="white"
+              position="absolute"
+              top="50%"
+              left="50%"
+              transform="translate(-50%,-50%)"
+            >
+              {photo.title}
+            </Text>
+          </Box>
+        ))}
+      </SimpleGrid>
+    </Flex>
   );
 };
